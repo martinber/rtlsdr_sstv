d8ddfa8a8c62f2f473dfacb25614e532b9646e84
Nano Remedi <agustoremedi@gmail.com>
2019-03-19 13:14:48 -0300

    Acomodado por martin

--- demod_FM_v2chilbert.py
+++ demod_FM_v2chilbert.py
@@ -5,63 +5,8 @@ import scipy.signal
 import matplotlib.pyplot as plt
 import pylab
 import math
-from funcion_tincho import escribir_pixel
+from funcion_tincho import escribir_pixel, filtrar
 from PIL import Image
-import collections
-
-def lowpass(cutout, delta_w, atten):
-    '''
-    cutout y delta_w en fracciones de pi radianes por segundo.
-
-    atten en decibeles positivos.
-    '''
-
-    beta = 0
-    if atten > 50:
-        beta = 0.1102 * (atten - 8.7)
-    elif atten < 21:
-        beta = 0
-    else:
-        beta = 0.5842 * (atten - 21)**0.4 + 0.07886 * (atten - 21)
-
-    length = math.ceil((atten - 8) / (2.285 * delta_w * math.pi)) + 1;
-    if length % 2 == 0:
-        length += 1
-
-    coeffs = np.kaiser(length, beta)
-
-    # i es el indice en el vector, n es el eje con el cero centrado en el medio
-    # del filtro
-    for i, n in enumerate(range(
-            int(-(length - 1) / 2),
-            int((length - 1) / 2)+1)):
-
-        if n == 0:
-            coeffs[i] *= cutout
-        else:
-            coeffs[i] *= math.sin(n * math.pi * cutout) / (n * math.pi)
-
-    return coeffs
-
-def filtrar(input, cutout, delta_w, atten):
-    '''
-    La derecha del buff tiene la muestra mas reciente y tiene el indice mas alto
-    '''
-
-    coeffs = lowpass(cutout, delta_w, atten)
-
-    #  plot(coeffs, numpy.abs(numpy.fft.fft(coeffs)))
-
-    buf = collections.deque([0] * len(coeffs))
-
-    for s in input:
-        buf.popleft()
-        buf.append(s)
-        sum = 0
-        for j in range(len(coeffs)):
-            sum += buf[-j - 1] * coeffs[j]
-
-        yield sum
 
 fs, data = wavfile.read('./audios_imagenes_prueba/pass_1_norm_7000.wav')
 img = Image.new('YCbCr',(640,496),"white")
@@ -152,9 +97,6 @@ while i < len(inst_fr):
                     escribir_pixel(img, columna, cont_linea, "nxt_lum", valor)
         except:
             break
-
-
-
 
         i+=int(0.1216*2*fs)
 
--- funcion_tincho.py
+++ funcion_tincho.py
@@ -1,4 +1,6 @@
-
+import collections
+import math
+import numpy as np
 
 def escribir_pixel(img, columna, linea, canal, valor):
     if linea >= img.height:
@@ -27,6 +29,60 @@ def escribir_pixel(img, columna, linea, canal, valor):
         prev = img.getpixel((columna,linea))
         datapixel = (int((valor-1500)/800*255), prev[1], prev[2])
         img.putpixel((columna,linea), datapixel)
+
+def lowpass(cutout, delta_w, atten):
+    '''
+    cutout y delta_w en fracciones de pi radianes por segundo.
+
+    atten en decibeles positivos.
+    '''
+
+    beta = 0
+    if atten > 50:
+        beta = 0.1102 * (atten - 8.7)
+    elif atten < 21:
+        beta = 0
+    else:
+        beta = 0.5842 * (atten - 21)**0.4 + 0.07886 * (atten - 21)
+
+    length = math.ceil((atten - 8) / (2.285 * delta_w * math.pi)) + 1;
+    if length % 2 == 0:
+        length += 1
+
+    coeffs = np.kaiser(length, beta)
+
+    # i es el indice en el vector, n es el eje con el cero centrado en el medio
+    # del filtro
+    for i, n in enumerate(range(
+            int(-(length - 1) / 2),
+            int((length - 1) / 2)+1)):
+
+        if n == 0:
+            coeffs[i] *= cutout
+        else:
+            coeffs[i] *= math.sin(n * math.pi * cutout) / (n * math.pi)
+
+    return coeffs
+
+def filtrar(input, cutout, delta_w, atten):
+    '''
+    La derecha del buff tiene la muestra mas reciente y tiene el indice mas alto
+    '''
+
+    coeffs = lowpass(cutout, delta_w, atten)
+
+    #  plot(coeffs, numpy.abs(numpy.fft.fft(coeffs)))
+
+    buf = collections.deque([0] * len(coeffs))
+
+    for s in input:
+        buf.popleft()
+        buf.append(s)
+        sum = 0
+        for j in range(len(coeffs)):
+            sum += buf[-j - 1] * coeffs[j]
+
+        yield sum
 
 if __name__ == "__main__":
     from PIL import Image as im
